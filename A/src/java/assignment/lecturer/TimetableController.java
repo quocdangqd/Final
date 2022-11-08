e672350c30888628c8b5b85a2ab458251525ff85
quocdangqd <maudoson456@gmail.com>
2022-11-08 21:55:23 +0700

    6

--- A/src/java/assignment/lecturer/TimetableController.java
+++ A/src/java/assignment/lecturer/TimetableController.java
@@ -2,7 +2,6 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
  */
-
 package assignment.lecturer;
 
 import dal.assignment.LecturerDBContext;
@@ -14,8 +13,10 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import java.util.ArrayList;
 import java.util.Date;
+import model.assignment.Account;
 import model.assignment.Lecturer;
 import model.assignment.Session;
 import model.assignment.TimeSlot;
@@ -26,59 +27,66 @@ import util.DateTimeHelper;
  * @author Hello Ngo Tung Son handsome
  */
 public class TimetableController extends HttpServlet {
-   
-    /** 
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code> methods.
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
      * @throws IOException if an I/O error occurs
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
-        int lid = Integer.parseInt(request.getParameter("lid"));
-        String raw_from = request.getParameter("from");
-        String raw_to = request.getParameter("to");
-        java.sql.Date from = null;
-        java.sql.Date to = null;
-        if(raw_from ==null || raw_from.length() ==0)
-        {
-            Date today = new Date();
-            int todayOfWeek = DateTimeHelper.getDayofWeek(today);
-            Date e_from = DateTimeHelper.addDays(today, 2 - todayOfWeek);
-            Date e_to = DateTimeHelper.addDays(today, 8-todayOfWeek);
-            from = DateTimeHelper.toDateSql(e_from);
-            to = DateTimeHelper.toDateSql(e_to);
+            throws ServletException, IOException {
+
+        HttpSession session = request.getSession();
+        Account ac = (Account) session.getAttribute("account");
+        boolean check = ac != null;
+        if (check) {
+            int lid = Integer.parseInt(request.getParameter("lid"));
+            String raw_from = request.getParameter("from");
+            String raw_to = request.getParameter("to");
+            java.sql.Date from = null;
+            java.sql.Date to = null;
+            if (raw_from == null || raw_from.length() == 0) {
+                Date today = new Date();
+                int todayOfWeek = DateTimeHelper.getDayofWeek(today);
+                Date e_from = DateTimeHelper.addDays(today, 2 - todayOfWeek);
+                Date e_to = DateTimeHelper.addDays(today, 8 - todayOfWeek);
+                from = DateTimeHelper.toDateSql(e_from);
+                to = DateTimeHelper.toDateSql(e_to);
+            } else {
+                from = java.sql.Date.valueOf(raw_from);
+                to = java.sql.Date.valueOf(raw_to);
+            }
+
+            request.setAttribute("from", from);
+            request.setAttribute("to", to);
+            request.setAttribute("dates", DateTimeHelper.getDateList(from, to));
+
+            TimeSlotDBContext slotDB = new TimeSlotDBContext();
+            ArrayList<TimeSlot> slots = slotDB.list();
+            request.setAttribute("slots", slots);
+
+            SessionDBContext sesDB = new SessionDBContext();
+            ArrayList<Session> sessions = sesDB.filter(lid, from, to);
+            request.setAttribute("sessions", sessions);
+
+            LecturerDBContext lecDB = new LecturerDBContext();
+            Lecturer lecturer = lecDB.get(lid);
+            request.setAttribute("lecturer", lecturer);
+
+            request.getRequestDispatcher("../view/lecturer/timetable.jsp").forward(request, response);
+        } else {
+            response.getWriter().println("Access Denied");
         }
-        else
-        {
-            from = java.sql.Date.valueOf(raw_from);
-            to = java.sql.Date.valueOf(raw_to);
-        }
-        
-        request.setAttribute("from", from);
-        request.setAttribute("to", to);
-        request.setAttribute("dates", DateTimeHelper.getDateList(from, to));
-        
-        TimeSlotDBContext slotDB = new TimeSlotDBContext();
-        ArrayList<TimeSlot> slots = slotDB.list();
-        request.setAttribute("slots", slots);
-        
-        SessionDBContext sesDB = new SessionDBContext();
-        ArrayList<Session> sessions = sesDB.filter(lid, from, to);
-        request.setAttribute("sessions", sessions);
-        
-        LecturerDBContext lecDB = new LecturerDBContext();
-        Lecturer lecturer = lecDB.get(lid);
-        request.setAttribute("lecturer", lecturer);
-        
-        request.getRequestDispatcher("../view/lecturer/timetable.jsp").forward(request, response);
-        
-    } 
+    }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
-    /** 
+    /**
      * Handles the HTTP <code>GET</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -86,12 +94,13 @@ public class TimetableController extends HttpServlet {
      */
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         processRequest(request, response);
-    } 
+    }
 
-    /** 
+    /**
      * Handles the HTTP <code>POST</code> method.
+     *
      * @param request servlet request
      * @param response servlet response
      * @throws ServletException if a servlet-specific error occurs
@@ -99,11 +108,8 @@ public class TimetableController extends HttpServlet {
      */
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response)
-    throws ServletException, IOException {
+            throws ServletException, IOException {
         processRequest(request, response);
     }
-
-    
-
 
 }
